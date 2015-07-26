d762840bf65fca5b9a9ba5930d1d796fa4cd6274
Punit Ganshani <punit.ganshani@gmail.com>
2015-07-26 19:12:24 +0800

    Updated version, clean up

--- NuGetViz/Properties/AssemblyInfo.cs
+++ NuGetViz/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NuGetViz")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("NuGet Vizualizer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Punit Ganshani")]
 [assembly: AssemblyProduct("NuGetViz")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Punit Ganshani 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
